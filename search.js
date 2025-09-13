480bc89c3717ba3dbc366e7f20bd4a1537dd2ced
tcgcollection <thomasklai88@gmail.com>
2025-09-13 15:40:57 -0700

    fix 2 different results

--- search.js
+++ search.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ------------------------
   const searchForm = document.getElementById("tcgIdForm");
   const cardQueryInput = document.getElementById("tcgIdQuery");
-  const resultsDiv = document.getElementById("cardResults");
+  const resultsDiv = document.getElementById("cardResult");
 
   searchForm?.addEventListener("submit", async (e) => {
     e.preventDefault(); // stop page refresh
