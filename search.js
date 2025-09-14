fa077a40c0f2a7ebc41c87e7f29334b71b9b9b62
tcgcollection <thomasklai88@gmail.com>
2025-09-13 22:33:10 -0700

    kjh

--- search.js
+++ search.js
@@ -110,15 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
               <p><strong>Set:</strong> ${c.set?.name || "Unknown"}</p>
               <p><strong>Rarity:</strong> ${c.rarity || "N/A"}</p>
               <p><strong>TCGPlayer Price - Normal:</strong> ${tcgplayerPriceNorm}</p>
-              // <p><strong>TCGPlayer Price - Holofoil:</strong> ${tcgplayerPriceHolo}</p>
-              // <p><strong>TCGPlayer Price - Reverse Holo:</strong> ${tcgplayerPriceReverse}</p>
 
-
-              // <p><strong>HP:</strong> ${c.hp || "N/A"}</p>
-              // <p><strong>Types:</strong> ${(c.types?.length ? c.types.join(", ") : "N/A")}</p>
-              // <p><strong>Ability:</strong> ${ability}</p>
-              // <p><strong>Weakness:</strong> ${weakness}</p>
-              // <p><strong>Resistance:</strong> ${resistance}</p>
               
             </div>
           `;
